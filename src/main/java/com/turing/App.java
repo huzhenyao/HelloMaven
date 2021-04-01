495a51ce9bfb0de170d8091d2bfe4391c3f71345
fred <fred@144.com>
2021-04-01 17:32:10 +0800

    第一次修改

---
+++ src/main/java/com/turing/App.java
@@ -0,0 +1,16 @@
+package com.turing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+        System.out.println("第一次修改!!!!");
+    }
+}
