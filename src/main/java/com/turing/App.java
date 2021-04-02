f1fccfd47b5ba4ad8ff0047788b91361125a8289
fred <fred@144.com>
2021-04-02 09:45:13 +0800

    dev

--- src/main/java/com/turing/App.java
+++ src/main/java/com/turing/App.java
@@ -11,8 +11,8 @@ public class App
 
         System.out.println( "Hello World!" );
 
-        System.out.println("第一次修改!!!!");
+        System.out.println("第一次提交!!!!");
 
-        System.out.println("我是dev");
+        System.out.println("我是main分支里的第一次修改");
     }
 }
